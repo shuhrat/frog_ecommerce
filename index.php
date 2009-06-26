05e0066ecb5b9d95cfec6a76cf4afb5bc6ab1c68
Chris Earls <cgearls@gmail.com>
2009-06-26 09:58:40 -0500

    cleanup

--- index.php
+++ index.php
@@ -8,14 +8,14 @@
  */
 
 Plugin::setInfos(array(
-    'id'          => 'ecommerce',
-    'title'       => 'Ecommerce',
-    'description' => 'Ecommerce solution for Frog CMS',
-    'version'     => '1.0.0',
-   	'license'     => 'AGPL',
-	'author'      => 'Chris Earls',
-    'website'     => 'http://www.chrisearls.net/',
-    'require_frog_version' => '0.9.5'
+	'id'		  => 'ecommerce',
+	'title'	 => 'Ecommerce',
+	'description' => 'Ecommerce solution for Frog CMS',
+	'version'	 => '1.0.0',
+	'license'	 => 'AGPL',
+	'author'	  => 'Chris Earls',
+	'website'	 => 'http://www.chrisearls.net/',
+	'require_frog_version' => '0.9.5'
 ));
 
 Plugin::addController('ecommerce', 'Ecommerce');
@@ -29,33 +29,29 @@ function collection($id) {
 	echo $ec->collection_show($id);
 }
 
-function cart_actions() {
-
-}
-
 function ecommerce_frontend($page)
 {
 	$ec = new EcommerceController;
 	include('classes/cart.class.php');
 	
 	ob_start();
-    $page->_executeLayout();
-    $output = ob_get_contents();
-    ob_end_clean();
-    
-    $uri_arr = explode('/',$page->url);
-    
-    if (in_array('products',$uri_arr)) {
-    	$cart = new Cart('shopping_cart');
-    	
+	$page->_executeLayout();
+	$output = ob_get_contents();
+	ob_end_clean();
+	
+	$uri_arr = explode('/',$page->url);
+	
+	if (in_array('products',$uri_arr)) {
+		$cart = new Cart('shopping_cart');
+		
 		// search
 		if (in_array('search',$uri_arr)) {
 			$keywords = !empty($_GET['keywords']) ? $_GET['keywords']: '';
 			$output = str_replace('<!-- ecommerce -->',$ec->products_search($keywords),$output);
 		}
 		
-    	// cart
-	    if (in_array('cart',$uri_arr)) {
+		// cart
+		if (in_array('cart',$uri_arr)) {
 			
 			// add item to cart
 			if ( !empty($_POST['variant_id']) && !empty($_POST['quantity']) ) {
@@ -82,35 +78,35 @@ function ecommerce_frontend($page)
 			if (!empty($_POST['variant_id']) || !empty($_POST['quantity']) || !empty($_POST['remove']))
 				$cart->save();
 			
-	    	$output = str_replace('<!-- ecommerce -->',$cart->display(),$output);
-	    }
-	    	
-	    // checkout
-	    if (in_array('checkout',$uri_arr))
-	    	if ($cart->hasItems())
-	    		$output = str_replace('<!-- ecommerce -->',$ec->checkout(),$output);
-	    	else
-	    		$output = str_replace('<!-- ecommerce -->','<p>You have no items in your cart.</p>',$output);
-    	
-    	//product display
-    	if (count($uri_arr) == 1)
-    		// products main page display
-    		$output = str_replace('<!-- ecommerce -->',$ec->products_all(),$output);
-    	else {
-	    	// display products by type
-	    	if (in_array('types',$uri_arr)) {
-	    		if ($page->slug == 'types')
-	    			$output = str_replace('<!-- ecommerce -->',$ec->product_types_nav(),$output);
-	    		else
-	    			$output = str_replace('<!-- ecommerce -->',$ec->products_by_type($page->slug),$output);
-		    }
-	    	// display products by vendor
-	    	else if (in_array('vendors',$uri_arr))
-	    		$output = str_replace('<!-- ecommerce -->',$ec->products_by_vendor($page->slug),$output);
-	    	// display product detail
-	    	else
-	    		$output = str_replace('<!-- ecommerce -->',$ec->product_show($page->slug),$output);
-	    }
+			$output = str_replace('<!-- ecommerce -->',$cart->display(),$output);
+		}
+			
+		// checkout
+		if (in_array('checkout',$uri_arr))
+			if ($cart->hasItems())
+				$output = str_replace('<!-- ecommerce -->',$ec->checkout(),$output);
+			else
+				$output = str_replace('<!-- ecommerce -->','<p>You have no items in your cart.</p>',$output);
+		
+		//product display
+		if (count($uri_arr) == 1)
+			// products main page display
+			$output = str_replace('<!-- ecommerce -->',$ec->products_all(),$output);
+		else {
+			// display products by type
+			if (in_array('types',$uri_arr)) {
+				if ($page->slug == 'types')
+					$output = str_replace('<!-- ecommerce -->',$ec->product_types_nav(),$output);
+				else
+					$output = str_replace('<!-- ecommerce -->',$ec->products_by_type($page->slug),$output);
+			}
+			// display products by vendor
+			else if (in_array('vendors',$uri_arr))
+				$output = str_replace('<!-- ecommerce -->',$ec->products_by_vendor($page->slug),$output);
+			// display product detail
+			else
+				$output = str_replace('<!-- ecommerce -->',$ec->product_show($page->slug),$output);
+		}
 		
 		//add cart actions
 		if ($cart->hasItems()) {
@@ -122,9 +118,9 @@ function ecommerce_frontend($page)
 			$cart_actions .= '</ul></div>';
 			$output = str_replace('<!-- ecommerce cart actions -->',$cart_actions,$output);
 		}
-    }
-    
-    // add ecommerce stylesheet to the frontend layout
+	}
+	
+	// add ecommerce stylesheet to the frontend layout
 	$output = str_replace('</head>','<link rel="stylesheet" href="/public/ecommerce/ecommerce.css" type="text/css" /></head>',$output);
 	
 	//add ecommerce js file
@@ -135,11 +131,11 @@ function ecommerce_frontend($page)
 		$output = str_replace('<!-- ecommerce flash -->',$ec->get_flash(),$output);
 	endif;
 	
-    // write frontend output
-    echo $output;
-    
+	// write frontend output
+	echo $output;
+	
 	// clear the flash
 	$_SESSION['ecommerce_flash'] = '';
 	
-    die();
+	die();
 }
--- javascripts/tagger.js
+++ javascripts/tagger.js
@@ -1,251 +1,250 @@
 /*
 Tagger - Converts any text input box into an easy-to-edit multiple-tag interface.
 */
-
 (function()
 {
-    var trim = function(str)
-    {
-        return str.replace(/^\s+|\s+$/g, '');
-    };
-    
-    var Tagger = function(element, options)
-    {
-        var obj = this;
-        var element = $(element);
-        var container = new Element('div');
-        var input = new Element('input');
-        
-        var config = {
-            'class':        'tagger',
-            inputAutosize:  true,
-            beforeAdd:      Prototype.emptyFunction,
-            afterAdd:       Prototype.emptyFunction,
-            beforeEdit:     Prototype.emptyFunction,
-            afterEdit:      Prototype.emptyFunction,
-            beforeDelete:   Prototype.emptyFunction,
-            afterDelete:    Prototype.emptyFunction
-        };
-        
-        Object.extend(config, options || {});
-        
-        this.getContainer = function()
-        {
-            return container;
-        };
-        
-        this.getConfig = function()
-        {
-            return config;
-        };
-        
-        this.addTag = function(tag)
-        {
-            if (false === config.beforeAdd(tag, obj)) return obj;
-            
-            if (tag.getContainer !== undefined) {
-                input.insert({before: tag.getContainer()});
-            }
-            
-            obj.updateRealInput();
-            config.afterAdd(tag, obj);
-            
-            return obj;
-        };
-        
-        if (config['class']) container.addClassName(config['class']);
-        element.hide();
-        element.insert({after: container});
-        container.insert(input);
-        
-        container.observe('click', function()
-        {
-            input.focus();
-        });
-        
-        this.insertCurrentInput = function()
-        {
-            var val = input.getValue();
-            
-            if (val) {
-                var tag = new Tag(obj);
-                tag.setTag(val);
-                obj.addTag(tag);
-            }
-            
-            input.clear();
-        };
-        
-        input.observe('keydown', function(event)
-        {
-            switch (event.keyCode) {
-                // comma, enter
-                case 188:
-                case 13:
-                    obj.insertCurrentInput();
-                    event.stop();
-                    break;
-            }
-        });
-        
-        input.observe('keypress', function(event)
-        {
-            // Detect backspace
-            if (event.keyCode != 8) return;
-            
-            if (!input.getValue()) {
-                // "click" last item
-                var lastTag = container.select('span.tag:last-of-type').invoke('fire', 'tag:click');
-                event.stop();
-            }
-        });
-        
-        input.observe('blur', obj.insertCurrentInput);
-        
-        this.autogrow = function()
-        {
-            if (true != config.inputAutosize) return;
-            var size = input.getValue().length + 1;
-            input.writeAttribute('size', size);
-        };
-        
-        this.getInput = function()
-        {
-            return input;
-        };
-        
-        this.createTag = function(tagName)
-        {
-            var tag = new Tag(obj);
-            tag.setTag(tagName);
-            obj.addTag(tag);
-        };
-        
-        this.updateRealInput = function()
-        {
-            var allTags = container.select('span.tag').pluck('innerHTML');
-            var tags = allTags.join(', ');
-            if (allTags == null)
-            	tags = '';
-            element.value = tags;
-            //element.writeAttribute('value', tags);
-        };
-        
-        input.observe('change', this.autogrow);
-        input.observe('keyup', this.autogrow);
-        
-        // Handle current value
-        var tagString = element.getValue();
-        if (tagString) {
-            var tags = tagString.split(',');
-            tags.each(function(tagName)
-            {
-                obj.createTag(tagName);
-            });
-        }
-    };
-    
-    var Tag = function(tagger)
-    {
-        var obj = this;
-        var tagger = tagger;
-        var container = new Element('span');
-        container.addClassName('tag');
-        
-        this.getContainer = function()
-        {
-            return container;
-        };
-        
-        this.remove = function()
-        {
-        	tagger.updateRealInput();
-            return container.remove();
-        };
-        
-        this.setTag = function(newTag)
-        {
-            tag = trim(newTag);
-            if (!tag) {
-                if (false === tagger.getConfig().beforeDelete(obj, tagger)) return;
-                
-                var previous = container.previous('span.tag');
-                if (previous) {
-                    previous.fire('tag:click');
-                } else {
-                    tagger.getInput().show().focus();
-                }
-                container.remove();
-                tagger.getConfig().afterDelete(obj, tagger);
-                
-                return;
-            }
-            tagger.getInput().show().focus();
-            container.innerHTML = tag;
-            
-            tagger.updateRealInput();
-        };
-        
-        this.edit = function(event)
-        {
-            if (false === tagger.getConfig().beforeEdit(obj, tagger)) return;
-            
-            tagger.getInput().hide();
-            var input = new Element('input', {type: 'text'});
-            
-            var doneEditing = function()
-            {
-                input.remove();
-                obj.setTag(input.getValue());
-                container.removeClassName('editing');
-                tagger.getConfig().afterEdit(obj, tagger);
-                tagger.updateRealInput();
-            };
-            
-            input.setStyle({width: container.getWidth() + 'px'});
-            input.value = container.innerHTML;
-            input.observe('blur', doneEditing);
-            input.observe('keydown', function(event)
-            {
-                switch (event.keyCode) {
-                    // comma, enter
-                    case 188:
-                    case 13:
-                        doneEditing();
-                        event.stop();
-                        break;
-                }
-            });
-            
-            input.observe('keypress', function(event)
-            {
-                // Backspace
-                if (event.keyCode != 8) return;
-                
-                if (!input.getValue()) {
-                    doneEditing();
-                    event.stop();
-                }
-            });
-            
-            input.observe('click', function(event)
-            {
-                event.stop();
-            });
-            
-            container.update(input);
-            container.addClassName('editing');
-            input.focus();
-            
-            event.stop();
-        };
-        
-        container.observe('click', this.edit);
-        container.observe('tag:click', this.edit);
-    };
-    
-    Tagger.Tag = Tag;
-    
-    if (!window.Virgen) window.Virgen = {};
-    window.Virgen.Tagger = Tagger;
+	var trim = function(str)
+	{
+	return str.replace(/^\s+|\s+$/g, '');
+	};
+	
+	var Tagger = function(element, options)
+	{
+	var obj = this;
+	var element = $(element);
+	var container = new Element('div');
+	var input = new Element('input');
+	
+	var config = {
+		'class':	'tagger',
+		inputAutosize:  true,
+		beforeAdd:	  Prototype.emptyFunction,
+		afterAdd:	   Prototype.emptyFunction,
+		beforeEdit:	 Prototype.emptyFunction,
+		afterEdit:	  Prototype.emptyFunction,
+		beforeDelete:   Prototype.emptyFunction,
+		afterDelete:	Prototype.emptyFunction
+	};
+	
+	Object.extend(config, options || {});
+	
+	this.getContainer = function()
+	{
+		return container;
+	};
+	
+	this.getConfig = function()
+	{
+		return config;
+	};
+	
+	this.addTag = function(tag)
+	{
+		if (false === config.beforeAdd(tag, obj)) return obj;
+		
+		if (tag.getContainer !== undefined) {
+		input.insert({before: tag.getContainer()});
+		}
+		
+		obj.updateRealInput();
+		config.afterAdd(tag, obj);
+		
+		return obj;
+	};
+	
+	if (config['class']) container.addClassName(config['class']);
+	element.hide();
+	element.insert({after: container});
+	container.insert(input);
+	
+	container.observe('click', function()
+	{
+		input.focus();
+	});
+	
+	this.insertCurrentInput = function()
+	{
+		var val = input.getValue();
+		
+		if (val) {
+		var tag = new Tag(obj);
+		tag.setTag(val);
+		obj.addTag(tag);
+		}
+		
+		input.clear();
+	};
+	
+	input.observe('keydown', function(event)
+	{
+		switch (event.keyCode) {
+		// comma, enter
+		case 188:
+		case 13:
+			obj.insertCurrentInput();
+			event.stop();
+			break;
+		}
+	});
+	
+	input.observe('keypress', function(event)
+	{
+		// Detect backspace
+		if (event.keyCode != 8) return;
+		
+		if (!input.getValue()) {
+		// "click" last item
+		var lastTag = container.select('span.tag:last-of-type').invoke('fire', 'tag:click');
+		event.stop();
+		}
+	});
+	
+	input.observe('blur', obj.insertCurrentInput);
+	
+	this.autogrow = function()
+	{
+		if (true != config.inputAutosize) return;
+		var size = input.getValue().length + 1;
+		input.writeAttribute('size', size);
+	};
+	
+	this.getInput = function()
+	{
+		return input;
+	};
+	
+	this.createTag = function(tagName)
+	{
+		var tag = new Tag(obj);
+		tag.setTag(tagName);
+		obj.addTag(tag);
+	};
+	
+	this.updateRealInput = function()
+	{
+		var allTags = container.select('span.tag').pluck('innerHTML');
+		var tags = allTags.join(', ');
+		if (allTags == null)
+			tags = '';
+		element.value = tags;
+		//element.writeAttribute('value', tags);
+	};
+	
+	input.observe('change', this.autogrow);
+	input.observe('keyup', this.autogrow);
+	
+	// Handle current value
+	var tagString = element.getValue();
+	if (tagString) {
+		var tags = tagString.split(',');
+		tags.each(function(tagName)
+		{
+		obj.createTag(tagName);
+		});
+	}
+	};
+	
+	var Tag = function(tagger)
+	{
+	var obj = this;
+	var tagger = tagger;
+	var container = new Element('span');
+	container.addClassName('tag');
+	
+	this.getContainer = function()
+	{
+		return container;
+	};
+	
+	this.remove = function()
+	{
+		tagger.updateRealInput();
+		return container.remove();
+	};
+	
+	this.setTag = function(newTag)
+	{
+		tag = trim(newTag);
+		if (!tag) {
+		if (false === tagger.getConfig().beforeDelete(obj, tagger)) return;
+		
+		var previous = container.previous('span.tag');
+		if (previous) {
+			previous.fire('tag:click');
+		} else {
+			tagger.getInput().show().focus();
+		}
+		container.remove();
+		tagger.getConfig().afterDelete(obj, tagger);
+		
+		return;
+		}
+		tagger.getInput().show().focus();
+		container.innerHTML = tag;
+		
+		tagger.updateRealInput();
+	};
+	
+	this.edit = function(event)
+	{
+		if (false === tagger.getConfig().beforeEdit(obj, tagger)) return;
+		
+		tagger.getInput().hide();
+		var input = new Element('input', {type: 'text'});
+		
+		var doneEditing = function()
+		{
+		input.remove();
+		obj.setTag(input.getValue());
+		container.removeClassName('editing');
+		tagger.getConfig().afterEdit(obj, tagger);
+		tagger.updateRealInput();
+		};
+		
+		input.setStyle({width: container.getWidth() + 'px'});
+		input.value = container.innerHTML;
+		input.observe('blur', doneEditing);
+		input.observe('keydown', function(event)
+		{
+		switch (event.keyCode) {
+			// comma, enter
+			case 188:
+			case 13:
+			doneEditing();
+			event.stop();
+			break;
+		}
+		});
+		
+		input.observe('keypress', function(event)
+		{
+		// Backspace
+		if (event.keyCode != 8) return;
+		
+		if (!input.getValue()) {
+			doneEditing();
+			event.stop();
+		}
+		});
+		
+		input.observe('click', function(event)
+		{
+		event.stop();
+		});
+		
+		container.update(input);
+		container.addClassName('editing');
+		input.focus();
+		
+		event.stop();
+	};
+	
+	container.observe('click', this.edit);
+	container.observe('tag:click', this.edit);
+	};
+	
+	Tagger.Tag = Tag;
+	
+	if (!window.Virgen) window.Virgen = {};
+	window.Virgen.Tagger = Tagger;
 })();
